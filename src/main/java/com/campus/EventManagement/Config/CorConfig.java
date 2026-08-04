f96abccbc5bcc913bfeb21c6407ef659812519cb
JamanRain <138153561+JamanRain@users.noreply.github.com>
2026-08-04 19:54:00 +0530

    Add allowed origin for CORS configuration

--- src/main/java/com/campus/EventManagement/Config/CorConfig.java
+++ src/main/java/com/campus/EventManagement/Config/CorConfig.java
@@ -17,7 +17,10 @@ class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://event-management-frontend-494a1nsp4-jamanrains-projects.vercel.app"
+                        )
                         .allowedMethods(
                                 "GET",
                                 "POST",
